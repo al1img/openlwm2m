4a09aa958a8b45704124c72018f6b0d81d0e50ed
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2019-05-07 18:43:48 +0300

    Add initial cmake files
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ example/linux/main.cpp
@@ -0,0 +1,9 @@
+#include "client.hpp"
+
+using namespace openlwm2m;
+
+int main() {
+    Client client;
+
+    return 0;
+}
---
+++ src/client.hpp
@@ -0,0 +1,10 @@
+#ifndef OPENLWM2M_CLIENT_HPP_
+#define OPENLWM2M_CLIENT_HPP_
+
+namespace openlwm2m {
+
+class Client {};
+
+}  // namespace openlwm2m
+
+#endif /* OPENLWM2M_CLIENT_HPP_ */
