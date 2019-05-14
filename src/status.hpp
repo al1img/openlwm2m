b36651d80e6a836d786497803ecba8075072330a
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2019-05-14 21:31:22 +0300

    Add status.hpp
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ src/status.hpp
@@ -0,0 +1,10 @@
+#ifndef OPENLWM2M_STATUS_HPP_
+#define OPENLWM2M_STATUS_HPP_
+
+namespace openlwm2m {
+
+enum class Status { STATUS_OK, STATUS_UNKNOWN_ERROR, STATUS_NOT_ENOUGH_MEM };
+
+}  // namespace openlwm2m
+
+#endif /* OPENLWM2M_STATUS_HPP_ */
